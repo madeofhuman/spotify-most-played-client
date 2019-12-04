a405f306254240cb31bf483b7b66f539e163cff6
CJ Odina <josephodina@gmail.com>
2019-12-04 02:13:30 +0100

    comment logic for clarity

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,44 +18,45 @@ const Home = () => {
 
   const getRefreshedAccessToken = async () => {
     const refreshToken = Cookies.get('_sp_refresh_token');
-    if (refreshToken && refreshToken.length) {
-      const { ok, data } = await refreshAccessToken(refreshToken);
-      if (ok) {
+    if (refreshToken && refreshToken.length) { // if there's a refresh token in the cookies
+      const { ok, data } = await refreshAccessToken(refreshToken); // use the refresh token to fetch a new access token
+      if (ok) { // if successful fetch
         Cookies.set('_sp_access_token', data.access_token);
         setState({ ...state, loading: false });
-      } else {
-        window.location.assign(oauthUrl);
+      } else { // fetching of access token with refresh token failed
+        window.location.assign(oauthUrl); // start a new oauth flow
       }
-    } else {
-      window.location.assign(oauthUrl);
+    } else { // there's no refresh token in the cookies
+      window.location.assign(oauthUrl); // start a new oauth flow
     }
   }
 
   const handleAuthHome = async () => {
     const accessToken = Cookies.get('_sp_access_token');
-    if (!accessToken || !accessToken.length) {
+    if (!accessToken || !accessToken.length) { // if there's no access token in cookie or it has empty value
       setState({ ...state, loading: true });
-      await getRefreshedAccessToken();
-    } else {
+      await getRefreshedAccessToken(); // fetch a new access token using the refresh token
+    } else { // if there's an access token in the cookie
       setState({ ...state, loading: true });
-      const { ok, data } = await fetchUser(accessToken);
-      if (ok) {
+      const { ok, data } = await fetchUser(accessToken); // fetch the user's profile data (also doubles as validating the access token)
+      if (ok) { // if successful fetch (aka access token is valid)
         setState({
           username: data.display_name.split(' ')[0],
           loading: false
         });
-      } else {
-        await getRefreshedAccessToken();
+      } else { // fetching of user profile failed (most likely token is invalid)
+        setState({ ...state, loading: true });
+        await getRefreshedAccessToken(); // fetch a new access token using the refresh token
         const accessToken = Cookies.get('_sp_access_token');
         setState({ ...state, loading: true });
-        const { ok, data } = await fetchUser(accessToken);
-        if (ok) {
+        const { ok, data } = await fetchUser(accessToken); // using the new access token, fetch the user's profile
+        if (ok) { // if successful fetch (aka access token is valid)
           setState({
             username: data.display_name.split(' ')[0],
             loading: false
           });
-        } else {
-          window.location.assign(oauthUrl);
+        } else { // fetching of user profile failed (most likely token is invalid)
+          window.location.assign(oauthUrl); // start a new oauth flow
         }
       }
     }
