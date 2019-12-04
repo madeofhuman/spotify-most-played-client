0917b075b34968975bdee4ab8fb404c431ffce55
CJ Odina <josephodina@gmail.com>
2019-12-04 02:36:53 +0100

    refactor auth method

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,23 +18,23 @@ const Home = () => {
 
   const getRefreshedAccessToken = async () => {
     const refreshToken = Cookies.get('_sp_refresh_token');
-    if (refreshToken && refreshToken.length) { // if there's a refresh token in the cookies
+    setState({ ...state, loading: true });
+    if (refreshToken && refreshToken.length > 0) { // if there's a refresh token in the cookies
       const { ok, data } = await refreshAccessToken(refreshToken); // use the refresh token to fetch a new access token
       if (ok) { // if successful fetch
         Cookies.set('_sp_access_token', data.access_token);
         setState({ ...state, loading: false });
       } else { // fetching of access token with refresh token failed
-        window.location.assign(oauthUrl); // start a new oauth flow
+        return window.location.assign(oauthUrl); // start a new oauth flow
       }
     } else { // there's no refresh token in the cookies
-      window.location.assign(oauthUrl); // start a new oauth flow
+      return window.location.assign(oauthUrl); // start a new oauth flow
     }
   }
 
   const handleAuthHome = async () => {
     const accessToken = Cookies.get('_sp_access_token');
     if (!accessToken || !accessToken.length) { // if there's no access token in cookie or it has empty value
-      setState({ ...state, loading: true });
       await getRefreshedAccessToken(); // fetch a new access token using the refresh token
     } else { // if there's an access token in the cookie
       setState({ ...state, loading: true });
@@ -45,7 +45,6 @@ const Home = () => {
           loading: false
         });
       } else { // fetching of user profile failed (most likely token is invalid)
-        setState({ ...state, loading: true });
         await getRefreshedAccessToken(); // fetch a new access token using the refresh token
         const accessToken = Cookies.get('_sp_access_token');
         setState({ ...state, loading: true });
@@ -56,7 +55,8 @@ const Home = () => {
             loading: false
           });
         } else { // fetching of user profile failed (most likely token is invalid)
-          window.location.assign(oauthUrl); // start a new oauth flow
+          setState({ ...state, loading: false });
+          return window.location.assign(oauthUrl); // start a new oauth flow
         }
       }
     }
